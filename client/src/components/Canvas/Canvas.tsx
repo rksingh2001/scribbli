870d6830cce232c37157148ac4b98e90b8a3c93b
Raunak Singh Jolly <rksj2001@gmail.com>
2023-08-06 16:06:55 +0530

    Testing Changes

--- client/src/components/Canvas/Canvas.tsx
+++ client/src/components/Canvas/Canvas.tsx
@@ -5,7 +5,7 @@ import useRoomId from "../../store/roomId";
 import useSocket from "../../store/socket";
 import useCanvasState from "../../store/canvasState";
 
-const Canvas = ({ height, width, disable } : {height: number, width: number, disable: boolean}) => {
+const Canvas = ({ height, width, disable }: { height: number, width: number, disable: boolean }) => {
   const [isPainting, setIsPainting] = useState(false);
   const roomId = useRoomId(state => state.roomId);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -13,6 +13,8 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
   const color = useCanvasState(state => state.color);
   const lineWidth = useCanvasState(state => state.lineWidth);
   const utilitySelected = useCanvasState(state => state.utilitySelected);
+
+  const oldPos: React.MutableRefObject<{posX: number, posY: number} | undefined> = useRef();
 
   useEffect(() => {
     socket.on("recieve_message", ({ pos, color, lineWidth }) => {
@@ -32,33 +34,45 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
     })
 
     socket.on('color-fill', ({ posX, posY, colorToFill }) => {
-      bfsColorFill(posX, posY, colorToFill)
+      // dfsColorFill(posX, posY, colorToFill)
     })
   }, [socket]);
 
-  const draw = (pos : { posX : number, posY : number}, color: string, lineWidth: number) => {
+  const draw = (pos: { posX: number, posY: number }, color: string, lineWidth: number) => {
     const context = canvasRef.current?.getContext('2d');
-    
+
     if (context) {
       context.lineWidth = lineWidth;
-      context.lineCap = "round";
+      // context.lineCap = "round";
       context.strokeStyle = color;
-      
+
       if (canvasRef.current === null) {
         console.error("Error: CanvasRef.current is null");
         return;
       }
-      
-      context.lineTo(pos.posX, pos.posY);
-      context.stroke();
+
       context.beginPath();
-      context.moveTo(pos.posX, pos.posY);
+      if (!oldPos || !oldPos.current) {
+        oldPos.current = pos;
+        return;
+      }
+      context.moveTo(oldPos.current.posX, oldPos.current.posY);
+      context.lineTo(pos.posX, pos.posY);
+      oldPos.current.posX = pos.posX;
+      oldPos.current.posY = pos.posY;
+      context.stroke();
+      context.closePath();
+
+      // context.lineTo(pos.posX, pos.posY);
+      // context.stroke();
+      // context.beginPath();
+      // context.moveTo(pos.posX, pos.posY);
     } else {
       throw new Error("Canvas context not found.");
     }
   }
 
-  const clearCanvas = () => { 
+  const clearCanvas = () => {
     const context = canvasRef.current?.getContext('2d');
 
     if (context) {
@@ -68,6 +82,116 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
     }
   }
 
+  const colorFloodFill = (x: number, y: number, colorToFill: string) => {
+    const ctx = canvasRef.current?.getContext('2d');
+    if (!ctx) {
+      throw new Error('Could not retrieve 2D context from canvas element');
+    }
+
+    x = Math.round(x);
+    y = Math.round(y);
+
+    const pixelStack: [number, number][] = [[x, y]];
+
+    const imageData: ImageData = ctx.getImageData(0, 0, width, height);
+
+    const startColor = getPixelColor(x, y);
+    const eraserColor: number[] = [0, 0, 0, 0];
+
+    if (compareColors(startColor, eraserColor) && utilitySelected === 'eraser') {
+      return;
+    }
+
+    while (pixelStack.length) {
+      const pos = pixelStack.pop();
+      if (!pos) return;
+
+      let posX = pos[0];
+      let posY = pos[1];
+
+      const color = getPixelColor(x, y);
+    }
+
+    function getPixelColor(x: number, y: number) {
+      const pixelDataIndex = (Math.round(y) * imageData.width + Math.round(x)) * 4;
+      const r = imageData.data[pixelDataIndex];
+      const g = imageData.data[pixelDataIndex + 1];
+      const b = imageData.data[pixelDataIndex + 2];
+      const a = imageData.data[pixelDataIndex + 3];
+
+      return [r, g, b, a];
+    }
+
+    function compareColors(color1: number[], color2: number[]): boolean {
+      return color1[0] === color2[0] && color1[1] === color2[1] && color1[2] === color2[2] && color1[3] === color2[3];
+    }
+  }
+
+  const dfsColorFill = (x: number, y: number, colorToFill: string) => {
+    const ctx = canvasRef.current?.getContext('2d');
+    if (!ctx) {
+      throw new Error('Could not retrieve 2D context from canvas element');
+    }
+
+    x = Math.round(x);
+    y = Math.round(y);
+
+    const imageData: ImageData = ctx.getImageData(0, 0, width, height);
+
+    const startColor = getPixelColor(x, y);
+    const replacementColor: number[] = [0, 0, 0, 0];
+
+    if (compareColors(startColor, replacementColor) && utilitySelected === 'eraser') {
+      return;
+    }
+
+    const pixelStack: { x: number; y: number }[] = [];
+    pixelStack.push({ x, y });
+
+    let uniquePixelValues = new Set<string>;
+
+    const start = performance.now();
+    while (pixelStack.length) {
+      const pixel = pixelStack.shift()!;
+      const { x, y } = pixel;
+      if (uniquePixelValues.has(`${x}|${y}`)) continue;
+
+      const color: number[] = getPixelColor(x, y);
+
+      if (compareColors(color, startColor)) {
+        ctx.fillStyle = colorToFill;
+        const off = 1;
+        ctx.fillRect(x, y, off, off);
+
+        uniquePixelValues.add(`${x}|${y}`);
+
+        const offset = 4;
+        for (let i = 1; i <= offset; ++i) {
+          if (x - i >= 0) pixelStack.push({ x: x - i, y });
+          if (x + i <= width - 1) pixelStack.push({ x: x + i, y });
+          if (y - i >= 0) pixelStack.push({ x, y: y - i });
+          if (y + i <= height - 1) pixelStack.push({ x, y: y + i });
+        }
+      }
+    }
+    const end = performance.now();
+
+    console.log(end-start);
+
+    function getPixelColor(x: number, y: number) {
+      const pixelDataIndex = (Math.round(y) * imageData.width + Math.round(x)) * 4;
+      const r = imageData.data[pixelDataIndex];
+      const g = imageData.data[pixelDataIndex + 1];
+      const b = imageData.data[pixelDataIndex + 2];
+      const a = imageData.data[pixelDataIndex + 3];
+
+      return [r, g, b, a];
+    }
+
+    function compareColors(color1: number[], color2: number[]): boolean {
+      return color1[0] === color2[0] && color1[1] === color2[1] && color1[2] === color2[2] && color1[3] === color2[3];
+    }
+  }
   const bfsColorFill = (x: number, y: number, colorToFill: string) => {
     const ctx = canvasRef.current?.getContext('2d');
     if (!ctx) {
@@ -76,41 +200,41 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
 
     x = Math.round(x);
     y = Math.round(y);
-    
+
     const imageData: ImageData = ctx.getImageData(0, 0, width, height);
 
     const startColor = getPixelColor(x, y);
     const replacementColor: number[] = [0, 0, 0, 0];
-    
+
     if (compareColors(startColor, replacementColor) && utilitySelected === 'eraser') {
       return;
     }
-    
+
     const pixelStack: { x: number; y: number }[] = [];
     pixelStack.push({ x, y });
-    
+
     let uniquePixelValues = new Set<string>;
-    
+
     while (pixelStack.length) {
       const pixel = pixelStack.pop()!;
       const { x, y } = pixel;
       if (uniquePixelValues.has(`${x}|${y}`)) continue;
 
       const color: number[] = getPixelColor(x, y);
-      
+
       if (compareColors(color, startColor)) {
         ctx.fillStyle = colorToFill;
         const off = 1;
-        ctx.fillRect(x-off, y-off, off*2, off*2);
+        ctx.fillRect(x - off, y - off, off * 2, off * 2);
 
-        uniquePixelValues.add(`${x}|${y}`)
+        uniquePixelValues.add(`${x}|${y}`);
 
         const offset = 2;
         for (let i = 1; i <= offset; ++i) {
-          if (x-i > 0) pixelStack.push({ x: x - i, y });
-          if (x+i < width - 1) pixelStack.push({ x: x + i, y });
-          if (y-i > 0) pixelStack.push({ x, y: y - i });
-          if (y+i < height - 1) pixelStack.push({ x, y: y + i });
+          if (x - i > 0) pixelStack.push({ x: x - i, y });
+          if (x + i < width - 1) pixelStack.push({ x: x + i, y });
+          if (y - i > 0) pixelStack.push({ x, y: y - i });
+          if (y + i < height - 1) pixelStack.push({ x, y: y + i });
         }
       }
     }
@@ -121,25 +245,25 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
       const g = imageData.data[pixelDataIndex + 1];
       const b = imageData.data[pixelDataIndex + 2];
       const a = imageData.data[pixelDataIndex + 3];
-      
+
       return [r, g, b, a];
     }
-    
+
     function compareColors(color1: number[], color2: number[]): boolean {
       return color1[0] === color2[0] && color1[1] === color2[1] && color1[2] === color2[2] && color1[3] === color2[3];
     }
   }
-  
-  const mouseDraw : MouseEventHandler<HTMLCanvasElement> = (e) => {
+
+  const mouseDraw: MouseEventHandler<HTMLCanvasElement> = (e) => {
     if (!isPainting) return;
-    
+
     const context = canvasRef.current?.getContext('2d');
 
     if (context) {
       context.lineWidth = lineWidth ?? 10;
-      context.lineCap = "round";
+      // context.lineCap = "round";
       context.strokeStyle = color;
-      
+
       if (canvasRef.current === null) {
         console.error("Error: CanvasRef.current is null");
         return;
@@ -158,7 +282,7 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
     }
   }
 
-  const handleMouseMove : MouseEventHandler<HTMLCanvasElement> = (e) => {
+  const handleMouseMove: MouseEventHandler<HTMLCanvasElement> = (e) => {
     if (!lineWidth) {
       return;
     }
@@ -179,7 +303,7 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
         const posY = e.clientY - offsetY;
 
         const pos = { posX, posY }
-        
+
         // We can transfer the data
         socket.emit("send_coordinates", { roomId, pos, color, lineWidth });
       }
@@ -200,14 +324,14 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
   }
 
   const handleMouseUp = () => {
-    if (!lineWidth) 
+    if (!lineWidth)
       return;
     socket.emit("mouse-up", { roomId });
     stopPainting();
   }
 
   const handleClick: MouseEventHandler<HTMLCanvasElement> = (e) => {
-    if(!canvasRef?.current || (utilitySelected !== 'eraser' && utilitySelected !== 'colorfill')) return;
+    if (!canvasRef?.current || (utilitySelected !== 'eraser' && utilitySelected !== 'colorfill')) return;
 
     const rect = canvasRef.current.getBoundingClientRect();
     const posX = Math.round(e.clientX - rect.left);
@@ -219,7 +343,7 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
 
     socket.emit('color-fill', { posX: posX, posY: posY, colorToFill: colorToFill, roomId: roomId });
     // @ts-ignore: colorToFill will never be undefined 
-    bfsColorFill(posX, posY, colorToFill);
+    dfsColorFill(posX, posY, colorToFill);
   }
 
   const startPainting = () => {
@@ -228,9 +352,10 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
 
   const stopPainting = () => {
     setIsPainting(false);
+    oldPos.current = undefined;
     const context = canvasRef.current?.getContext('2d');
     if (context) {
-      context.beginPath();
+      context.closePath();
     } else {
       throw console.error("Context Not Found");
     }
@@ -238,25 +363,25 @@ const Canvas = ({ height, width, disable } : {height: number, width: number, dis
 
   return (
     disable ?
-    <canvas
-      id="canvas" 
-      width={width}
-      height={height}
-      ref={canvasRef}
-      style={{ backgroundColor: "white" }}
-    /> 
-    :
-    <canvas 
-      id="canvas" 
-      width={width}
-      height={height}
-      ref={canvasRef}
-      onClick={handleClick}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
-      style={{ backgroundColor: "white" }}
-    />
+      <canvas
+        id="canvas"
+        width={width}
+        height={height}
+        ref={canvasRef}
+        style={{ backgroundColor: "white" }}
+      />
+      :
+      <canvas
+        id="canvas"
+        width={width}
+        height={height}
+        ref={canvasRef}
+        onClick={handleClick}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        style={{ backgroundColor: "white" }}
+      />
   )
 }
 
