07736ef15449939244856ee1c5196715f6a230c3
Raunak Singh Jolly <rksj2001@gmail.com>
2023-08-15 16:49:07 +0530

    Showing colors in utility widgets, when they are selected

--- client/src/components/UtilitiesWidget/UtilitiesWidget.tsx
+++ client/src/components/UtilitiesWidget/UtilitiesWidget.tsx
@@ -1,11 +1,13 @@
 import './UtilitiesWidget.scss';
 import { BiSolidEraser, BiSolidColorFill } from "react-icons/bi";
 import useCanvasState from '../../store/canvasState';
+import usePlayerList from '../../store/playerList';
 
 const UtilitiesWidget = ({ width, height }: { width: number, height: number }) => {
   const utilitySelected = useCanvasState(state => state.utilitySelected);
   const setUtilitySelected = useCanvasState(state => state.setUtilitySelected);
   const setLineWidth = useCanvasState(state => state.setLineWidth);
+  const color = useCanvasState(state => state.color);
 
   const handleClick = (utilityName: string) => {
     setUtilitySelected(utilityName);
@@ -15,10 +17,10 @@ const UtilitiesWidget = ({ width, height }: { width: number, height: number }) =
   return (
     <div className='utilites-widget' style={{ width: width, height: height }}>
       <div onClick={() => handleClick('eraser')} style={{ width: width, height: height / 2 }}>
-        {utilitySelected === 'eraser' ? <BiSolidEraser size="large" /> : <BiSolidEraser size="large" />}
+        {utilitySelected === 'eraser' ? <BiSolidEraser size="large" color="#0e7490" /> : <BiSolidEraser size="large" />}
       </div>
       <div onClick={() => handleClick('colorfill')} style={{ width: width, height: height / 2 }}>
-        {utilitySelected === 'colorfill' ? <BiSolidColorFill size="large" /> : <BiSolidColorFill size="large" />}
+        {utilitySelected === 'colorfill' ? <BiSolidColorFill size="large" color={color} /> : <BiSolidColorFill size="large" />}
       </div>
     </div>
   )
