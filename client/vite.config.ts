e36e71dd91965b14e808aa43eb62d6640b11e60f
Raunak Singh Jolly <rksj2001@gmail.com>
2023-08-27 15:57:41 +0530

    Set server.hmr.overlay to false, so as to not get the uri error, doesn't seem to affect the game as of now

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,4 +6,9 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  server: {
+    hmr: {
+      overlay: false
+    }
+  }
 });
