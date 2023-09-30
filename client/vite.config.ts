5571e7be974fab0dc8be097ca6eda336f142cd91
raunak <rksj2001@gmail.com>
2023-09-30 18:02:37 +0530

    fixed production build issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   server: {
     hmr: {
